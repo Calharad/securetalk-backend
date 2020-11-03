3c6c830c99b6641a661942a7ea8c79b98983c7dd
Calharad <adudek0610@gmail.com>
2020-11-03 18:23:04 +0100

    project init

---
+++ src/test/java/pl/calharad/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package pl.calharad;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
